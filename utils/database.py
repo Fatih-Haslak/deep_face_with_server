91bd435cca5c76cfc56a5ed9646c520f4d951305
Fatih HAŞLAK <62775596+Fatih-Haslak@users.noreply.github.com>
2023-08-03 21:11:17 +0300

    added formatter

--- utils/database.py
+++ utils/database.py
@@ -1,12 +1,14 @@
 import sqlite3
 import time
 
-class ClassDataBase():
+
+class ClassDataBase:
     def __init__(self):
         conn = sqlite3.connect("database/user_stats.db")
         cursor = conn.cursor()
-        
-        cursor.execute("""
+
+        cursor.execute(
+            """
         CREATE TABLE IF NOT EXISTS user_stats (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             username TEXT NOT NULL,
@@ -14,33 +16,41 @@ class ClassDataBase():
             model_output TEXT NOT NULL,
             execution_time REAL NOT NULL
         )
-    """)
+    """
+        )
         conn.commit()
         conn.close()
 
-    def return_data_from_db(self,username:str):
+    def return_data_from_db(self, username: str):
         conn = sqlite3.connect("database/user_stats.db")
         cursor = conn.cursor()
 
-        cursor.execute("""
+        cursor.execute(
+            """
             SELECT timestamp, model_output, execution_time
             FROM user_stats
             WHERE username=?
-        """, (username,))
+        """,
+            (username,),
+        )
 
         result = cursor.fetchall()
         conn.close()
         return result
 
-
-    def save_model_output_to_db(self,username:str, model_output:str, execution_time:str):
-                conn = sqlite3.connect("database/user_stats.db")
-                cursor = conn.cursor()
-                timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
-                cursor.execute("""
+    def save_model_output_to_db(
+        self, username: str, model_output: str, execution_time: str
+    ):
+        conn = sqlite3.connect("database/user_stats.db")
+        cursor = conn.cursor()
+        timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
+        cursor.execute(
+            """
                     INSERT INTO user_stats (username, timestamp, model_output, execution_time)
                     VALUES (?, ?, ?, ?)
-                """, (username, timestamp, model_output, execution_time))
+                """,
+            (username, timestamp, model_output, execution_time),
+        )
 
-                conn.commit()
-                conn.close()
+        conn.commit()
+        conn.close()
--- utils/face_recognition.py
+++ utils/face_recognition.py
@@ -1,11 +1,10 @@
 from deepface import DeepFace
 
-class ClassDeepFace():
+
+class ClassDeepFace:
     def __init__(self):
         pass
 
-    def run(self,image1,image2):
-        result  = DeepFace.verify(image1, image2)
+    def run(self, image1, image2):
+        result = DeepFace.verify(image1, image2)
         return result
-    
-
